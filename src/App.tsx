fbb693b70f025bbde0b53be95d7693515a296d54
feddorovich <feddorovich@outlook.com>
2023-02-03 12:20:58 +0300

    01 nested components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Body } from './site/Body';
+import {Header} from "./site/Header";
+import {Footer} from "./site/Footer";
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <>
+        <Header title={'New Header'}/>
+        <Body titleForBody={'New Body'}/>
+        <Footer titleForFooter={'New Footer'}/>
+    </>
   );
 }
 
---
+++ src/site/Footer.tsx
@@ -0,0 +1,12 @@
+
+type FooterType = {
+    titleForFooter: string
+}
+
+export const Footer = (props: FooterType) => {
+    return (
+        <div>
+            {props.titleForFooter}
+        </div>
+    )
+}
