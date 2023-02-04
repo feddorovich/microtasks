fba412c6f99ce9df564f5af539bdd36014de9604
feddorovich <feddorovich@outlook.com>
2023-02-04 13:26:08 +0300

    training

---
+++ src/00-training/03-03-button/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import {Button} from "./components/Button";
+
+function App(): JSX.Element {
+
+    const funToLog = (props1: string, props2: number) => {
+        console.log(props1, props2)
+    }
+
+    return (
+        <div className={'App'}>
+            <Button title={'Button'} callback={() => funToLog('Some text to log', 50)}/>
+            <Button title={'Button 2'} callback={() => funToLog('New text to log', 500)}/>
+        </div>
+    );
+}
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Button} from "./03-03-button/components/Button";
 
 function App(): JSX.Element {
 
-    const funToLog = (props1: string, props2: number) => {
-        console.log(props1, props2)
-    }
-
     return (
         <div className={'App'}>
-            <Button title={'Button'} callback={() => funToLog('Some text to log', 50)}/>
-            <Button title={'Button 2'} callback={() => funToLog('New text to log', 500)}/>
         </div>
     );
 }
