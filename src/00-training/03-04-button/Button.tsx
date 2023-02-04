8d788f8d4ae7b15fffe16b1e2fdc91da626477c3
feddorovich <feddorovich@outlook.com>
2023-02-04 13:36:53 +0300

    03 button

---
+++ src/00-training/03-04-button/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type ButtonType = {
+    title: string
+    callBack: () => void
+}
+
+export const Button = (props: ButtonType) => {
+
+    const onClickHandler = () => {
+        props.callBack()
+    }
+
+    return (
+        <div>
+            <button onClick={onClickHandler}>{props.title}</button>
+        </div>
+    );
+};
